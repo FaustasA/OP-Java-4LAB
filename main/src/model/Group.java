3cfd2249a06fde238d73178fa4d1155ae6b5ceb0
Faustas Avulis <faustasavulis@gmail.com>
2025-12-03 13:05:56 +0200

    Added class Group

---
+++ main/src/model/Group.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Group {
+
+    private String name;
+
+    public Group(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
